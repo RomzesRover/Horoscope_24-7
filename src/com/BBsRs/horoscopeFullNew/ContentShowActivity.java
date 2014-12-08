19bab633d20ac7d9d7833fc60f3020c74f83dc27
Roman <RomzesRover@gmail.com>
2014-12-08 20:16:15 +0500

    disable inverse text when toggled

--- src/com/BBsRs/horoscopeFullNew/ContentShowActivity.java
+++ src/com/BBsRs/horoscopeFullNew/ContentShowActivity.java
@@ -39,16 +39,16 @@ public class ContentShowActivity extends Activity {
         sliderMenu = addonSlider().obtainDefaultSliderMenu(R.layout.menu);
         addonSlider().setOverlayActionBar(false);
         sliderMenu.add(getResources().getString(R.string.mail_ru_title).toUpperCase()).setCustomLayout(R.layout.custom_slider_menu_item).clickable(false).setTextAppereance(1);
-        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[0], MailRuYesterdayLoaderFragment.class, SliderMenu.BLUE);
-        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[1], MailRuTodayLoaderFragment.class, SliderMenu.BLUE);
-        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[2], MailRuTomorrowLoaderFragment.class, SliderMenu.BLUE);
-        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[3], MailRuPersonalLoaderFragment.class, SliderMenu.BLUE);
-        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[4], MailRuWeekLoaderFragment.class, SliderMenu.BLUE);
-        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[5], MailRuMonthLoaderFragment.class, SliderMenu.BLUE);
-        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[6], MailRuYearLoaderFragment.class, SliderMenu.BLUE);
+        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[0], MailRuYesterdayLoaderFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
+        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[1], MailRuTodayLoaderFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
+        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[2], MailRuTomorrowLoaderFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
+        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[3], MailRuPersonalLoaderFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
+        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[4], MailRuWeekLoaderFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
+        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[5], MailRuMonthLoaderFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
+        sliderMenu.add(getResources().getStringArray(R.array.mail_ru_horoscopes)[6], MailRuYearLoaderFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
         sliderMenu.add(getResources().getString(R.string.application_title).toUpperCase()).setCustomLayout(R.layout.custom_slider_menu_item).clickable(false).setTextAppereance(1);
-        sliderMenu.add(getResources().getStringArray(R.array.application_titles)[0], SettingsFragment.class, SliderMenu.BLUE);
-        sliderMenu.add(getResources().getStringArray(R.array.application_titles)[1], AboutFragment.class, SliderMenu.BLUE);
+        sliderMenu.add(getResources().getStringArray(R.array.application_titles)[0], SettingsFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
+        sliderMenu.add(getResources().getStringArray(R.array.application_titles)[1], AboutFragment.class, SliderMenu.BLUE).setTextAppereanceInverse(1);
         //setting up for initial page today horoscope! 
         sliderMenu.setCurrentPage(2);
         
