816852dd3d27f5d5725a4bb1d68767fff7574fa6
Roman <RomzesRover@gmail.com>
2014-12-09 15:43:16 +0500

    add automatic sign calculator

--- src/com/BBsRs/horoscopeFullNew/SettingsFragment.java
+++ src/com/BBsRs/horoscopeFullNew/SettingsFragment.java
@@ -5,10 +5,12 @@ import java.util.Calendar;
 
 import org.holoeverywhere.preference.DatePreference;
 import org.holoeverywhere.preference.DatePreference.OnDateSetListener;
+import org.holoeverywhere.preference.ListPreference;
 import org.holoeverywhere.preference.PreferenceFragment;
 import org.holoeverywhere.preference.PreferenceManager;
 import org.holoeverywhere.preference.SharedPreferences;
 import org.holoeverywhere.preference.SharedPreferences.Editor;
+import org.holoeverywhere.widget.Toast;
 
 import android.os.Bundle;
 
@@ -40,6 +42,7 @@ public class SettingsFragment extends PreferenceFragment {
         
         //setting up date change preference, cuz we need save date.
         DatePreference myDatePref = (DatePreference) findPreference("preference_date_born");
+        final ListPreference myListPref = (ListPreference) findPreference("preference_zodiac_sign");
         
         Calendar cal=Calendar.getInstance();
         myDatePref.setDay(sPref.getInt("dayBorn", cal.get(Calendar.DAY_OF_MONTH)));
@@ -54,7 +57,10 @@ public class SettingsFragment extends PreferenceFragment {
 				Editor ed = sPref.edit();   
 				ed.putInt("dayBorn", day);				
 			    ed.putInt("monthBorn", month);				
-			    ed.putInt("yearBorn", year);		
+			    ed.putInt("yearBorn", year);	
+			    ed.putString("preference_zodiac_sign", zodiacNumber(day, month+1));
+			    myListPref.setValue(zodiacNumber(day, month+1));
+			    Toast.makeText(getActivity(), getResources().getString(R.string.automatic_determined_sign)+" "+getResources().getStringArray(R.array.zodiac_signs)[Integer.parseInt(zodiacNumber(day, month+1))], Toast.LENGTH_LONG).show();
 				ed.commit();
 				return false;
 			}
@@ -67,4 +73,33 @@ public class SettingsFragment extends PreferenceFragment {
         getSupportActionBar().setTitle(R.string.app_name);
         getSupportActionBar().setSubtitle(R.string.preference);
     }
+    
+	String zodiacNumber (int day, int month){
+		if((month == 1) && (day <= 20) || (month == 12) && (day >= 22)) {
+			return "9";
+		 } else if((month == 1) || (month == 2) && (day <= 19)) {
+			 return "10";
+		 } else if((month == 2) || (month == 3) && (day <= 20)) {
+			 return "11";
+		 } else if((month == 3) || (month == 4) && (day <= 19)) {
+			 return "0";
+		 } else if((month == 4) || (month == 5) && (day <= 21)) {
+			 return "1";
+		 } else if((month == 5) || (month == 6) && (day <= 21)) {
+			 return "2";
+		 } else if((month == 6) || (month == 7) && (day <= 23)) {
+			 return "3";
+		 } else if((month == 7) || (month == 8) && (day <= 23)) {
+			 return "4";
+		 } else if((month == 8) || (month == 9) && (day <= 23)) {
+			 return "5";
+		 } else if((month == 9) || (month == 10) && (day <= 23)) {
+			 return "6";
+		 } else if((month == 10) || (month == 11) && (day <= 22)) {
+			 return "7";
+		 } else if(month == 12) {
+			 return "8";
+		 }
+		 return "0";
+	}
 }
