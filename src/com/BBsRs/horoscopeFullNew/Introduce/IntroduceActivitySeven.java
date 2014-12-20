6f3c34fcce52c6e2e00064bf0ff58b7d3d3cba44
Roman <RomzesRover@gmail.com>
2014-12-20 17:46:33 +0500

    adding six and seven steps to intro

--- src/com/BBsRs/horoscopeFullNew/Introduce/IntroduceActivitySeven.java
+++ src/com/BBsRs/horoscopeFullNew/Introduce/IntroduceActivitySeven.java
@@ -2,11 +2,7 @@ package com.BBsRs.horoscopeFullNew.Introduce;
 
 import org.holoeverywhere.preference.PreferenceManager;
 import org.holoeverywhere.preference.SharedPreferences;
-import org.holoeverywhere.preference.SharedPreferences.Editor;
-import org.holoeverywhere.widget.AdapterView;
-import org.holoeverywhere.widget.AdapterView.OnItemSelectedListener;
 import org.holoeverywhere.widget.Button;
-import org.holoeverywhere.widget.Spinner;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,15 +12,10 @@ import com.BBsRs.horoscopeFullNew.ActivityLoader;
 import com.BBsRs.horoscopeFullNew.R;
 import com.BBsRs.horoscopeFullNew.Base.BaseActivity;
 
-public class IntroduceActivityFive extends BaseActivity {
+public class IntroduceActivitySeven extends BaseActivity {
 	
 	//preferences 
     SharedPreferences sPref;
-    
-    int check=0;
-    
-    //
-    int k=0;
     
 	/** Called when the activity is first created. */
 	@Override
@@ -37,10 +28,7 @@ public class IntroduceActivityFive extends BaseActivity {
 	    //set app lang
         setLocale(sPref.getString("preference_locales", getResources().getString(R.string.default_locale)));
         
-        if (!sPref.getString("preference_locales", getResources().getString(R.string.default_locale)).equals("ru"))
-        	k=2;
-        
-	    this.setContentView(R.layout.activity_inroduce_five);
+	    this.setContentView(R.layout.activity_inroduce_seven);
 	    
 	    //init butts
 	    Button back = (Button)this.findViewById(R.id.buttonBack);
@@ -50,7 +38,7 @@ public class IntroduceActivityFive extends BaseActivity {
 	    next.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent refresh = new Intent(getApplicationContext(), IntroduceActivitySix.class);
+				Intent refresh = new Intent(getApplicationContext(), ActivityLoader.class);
 				//restart activity
 			    startActivity(refresh);   
 			    //set  animation
@@ -65,7 +53,7 @@ public class IntroduceActivityFive extends BaseActivity {
 	    back .setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent refresh = new Intent(getApplicationContext(), IntroduceActivityFour.class);
+				Intent refresh = new Intent(getApplicationContext(), IntroduceActivitySix.class);
 				//restart activity
 			    startActivity(refresh);   
 			    //set  animation
@@ -74,24 +62,6 @@ public class IntroduceActivityFive extends BaseActivity {
 			    finish();
 			}
 		});
-	    
-	    final Spinner provider = (Spinner)findViewById(R.id.spinnerProvider);
-	    provider.setSelection(Integer.parseInt(sPref.getString("preference_provider", getResources().getString(R.string.default_provider)))-k);
-	    
-	    provider.setOnItemSelectedListener(new OnItemSelectedListener(){    
-	    	@Override
-	    	public void onItemSelected(AdapterView adapter, View v, int i, long lng) {
-	    		check++;
-	    		if(check>1){
-	    		Editor ed = sPref.edit();   
-	    		ed.putString("preference_provider", getResources().getStringArray(R.array.providers_entryValues)[i+k]); 	
-	    		ed.commit();
-	    		}
-	    	} 
-	    	@Override     
-	    	public void onNothingSelected(AdapterView<?> parentView) {}
-	    }); 
-	    
 	}
 
 }
