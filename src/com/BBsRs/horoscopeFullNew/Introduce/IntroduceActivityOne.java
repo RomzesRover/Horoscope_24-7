bafd061f03bcc833d6d6738dfca99eac11297be1
Roman Gaitbaev <RomzesRover@gmail.com>
2015-05-28 05:45:06 +0500

    delete user agreement

--- src/com/BBsRs/horoscopeFullNew/Introduce/IntroduceActivityOne.java
+++ src/com/BBsRs/horoscopeFullNew/Introduce/IntroduceActivityOne.java
@@ -8,19 +8,13 @@ import org.holoeverywhere.widget.AdapterView;
 import org.holoeverywhere.widget.AdapterView.OnItemSelectedListener;
 import org.holoeverywhere.widget.Button;
 import org.holoeverywhere.widget.Spinner;
-import org.holoeverywhere.widget.TextView;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannableStringBuilder;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.view.View;
-import android.widget.TextView.BufferType;
 
-import com.BBsRs.horoscopeNewEdition.R;
 import com.BBsRs.horoscopeFullNew.Base.BaseActivity;
+import com.BBsRs.horoscopeNewEdition.R;
 
 public class IntroduceActivityOne extends BaseActivity {
 	
@@ -96,48 +90,6 @@ public class IntroduceActivityOne extends BaseActivity {
 	    	public void onNothingSelected(AdapterView<?> parentView) {}
 	    }); 
 	    
-	    //set license
-	    TextView text = (TextView)this.findViewById(R.id.textAgreement);
-	    String sentence = getResources().getString(R.string.clickable_string);
-
-
-	    text.setMovementMethod(LinkMovementMethod.getInstance());
-	    text.setText(addClickablePart(sentence), BufferType.SPANNABLE);
-	}
-	
-	private SpannableStringBuilder addClickablePart(String str) {
-	    SpannableStringBuilder ssb = new SpannableStringBuilder(str);
-
-	    int idx1 = str.indexOf("[");
-	    int idx2 = 0;
-	    while (idx1 != -1) {
-	        idx2 = str.indexOf("]", idx1) + 1;
-
-	        final String clickString = str.substring(idx1, idx2);
-	        ssb.setSpan(new ClickableSpan() {
-
-	            @Override
-	            public void onClick(View widget) {
-	            	showLicenseDialog();
-	            }
-	        }, idx1, idx2, 0);
-	        idx1 = str.indexOf("[", idx2);
-	    }
-
-	    return ssb;
-	 }
-	
-	
-	private void showLicenseDialog(){
-		final Context context = IntroduceActivityOne.this; 								// create context
-		
-		AlertDialog.Builder build = new AlertDialog.Builder(context); 				// create build for alert dialog
-		build.setTitle(getResources().getString(R.string.agreement_top)); 					// set title
-		build.setMessage(getApplicationContext().getResources().getString(R.string.agreement));
-		build.setCancelable(true);
-		build.setNegativeButton("Ok", null);
-		alert = build.create();															// show dialog
-		alert.show();
 	}
 	
 	private void activityRefresh(){
