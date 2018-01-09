db006636b0b10856cfad403de44539c8926981c8
Roman Loginov <RomzesRover@gmail.com>
2018-01-09 13:07:16 +0500

    add base theme style

--- src/com/BBsRs/horoscopeNewEdition/ContentActivity.java
+++ src/com/BBsRs/horoscopeNewEdition/ContentActivity.java
@@ -1,16 +1,24 @@
 package com.BBsRs.horoscopeNewEdition;
 
-import org.holoeverywhere.app.Activity;
-
 import android.os.Bundle;
 
+import com.BBsRs.horoscopeNewEdition.Base.BaseActivity;
 
 
-public class ContentActivity extends Activity {
+
+public class ContentActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+        
+        getSupportActionBar().setIcon(R.drawable.ic_menu);
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        setTitle("Hello");
     }
 }
